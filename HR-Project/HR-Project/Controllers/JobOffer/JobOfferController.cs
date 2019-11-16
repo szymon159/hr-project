a8934da43a842d299cb169364220c1de551bf511
Szymon Stasiak <szymon-stasiak@outlook.com>
2019-11-16 16:11:42 +0100

    Deleted unused views/models/controlelrs from inital project, created sample job offers view

---
+++ HR-Project/HR-Project/Controllers/JobOffer/JobOfferController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HR_Project.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HR_Project.Controllers
+{
+    public class JobOfferController : Controller
+    {
+        private static List<JobOfferViewModel> jobOffers = new List<JobOfferViewModel>
+        {
+            new JobOfferViewModel{Id = 1, JobTitle = "BackendDeveloper"},
+        };
+
+        public IActionResult Index()
+        {
+            return View(jobOffers);
+        }
+    }
+}
