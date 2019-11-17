aa44ee83ec8eabfa6cd354c3959658dd30678bbb
Szymon Stasiak <szymon-stasiak@outlook.com>
2019-11-17 02:08:47 +0100

    Implemented SearchBar view

---
+++ HR-Project/ViewModels/SearchViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HR_Project.ViewModels
+{
+    public class SearchViewModel
+    {
+        public string SearchString { get; set; }
+        public string ComponentTitle { get; set; }
+
+        public SearchViewModel(object text)
+        {
+            ComponentTitle = text as string;
+        }
+    }
+}
