a7df126c9a6df8b52e7a15855f636025760f69b2
Szymon Stasiak <szymon-stasiak@outlook.com>
2019-11-24 21:25:50 +0100

    Implemented modifying job offers

---
+++ HR-Project/ViewModels/JobOffer/JobOfferDetailsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HR_Project.ViewModels
+{
+    public class JobOfferDetailsViewModel
+    {
+        public JobOfferViewModel JobOfferModel { get; set; }
+
+        public bool IsEditing { get; set; }
+    }
+}
