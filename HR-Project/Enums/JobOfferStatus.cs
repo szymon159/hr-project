bd697226bcc21e1e31a67032d978737bdb05eeee
Szymon Stasiak <szymon-stasiak@outlook.com>
2019-11-24 21:15:13 +0100

    Implemented JobOfferStatus

---
+++ HR-Project/Enums/JobOfferStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HR_Project.Enums
+{
+    public enum JobOfferStatus
+    {
+        Active,
+        Inactive
+    }
+}
---
+++ HR-Project/ViewModels/JobOffer/JobOfferViewModel.cs
@@ -0,0 +1,21 @@
+﻿using HR_Project.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HR_Project.ViewModels
+{
+    public class JobOfferViewModel
+    {
+        public int? Id { get; set; }
+
+        public string JobTitle { get; set; }
+
+        public string Description { get; set; }
+
+        public JobOfferStatus Status {get; set; }
+
+        public string StatusCssClass { get { return "joboffer-status-" + Status.ToString().ToLower(); } }
+    }
+}
